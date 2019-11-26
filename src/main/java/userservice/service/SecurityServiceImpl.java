d47a1b34df0b4338c4f0b3a237fe201710f54d8b
Ravi Sanka <33692085+raviksanka@users.noreply.github.com>
2019-11-26 18:40:09 +0530

    Create SecurityServiceImpl.java

---
+++ src/main/java/userservice/service/SecurityServiceImpl.java
@@ -0,0 +1,25 @@
+package userservice.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService{
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+    @Override
+    public String findLoggedInUsername() {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails)userDetails).getUsername();
+        }
+        return null;
+    }
+}
