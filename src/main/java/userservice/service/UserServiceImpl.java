256606e5c3b2f03d0602a25a367699ad9cc3e6d1
Ravi Sanka <33692085+raviksanka@users.noreply.github.com>
2019-11-26 17:57:49 +0530

    Update UserServiceImpl.java

--- src/main/java/userservice/service/UserServiceImpl.java
+++ src/main/java/userservice/service/UserServiceImpl.java
@@ -1,11 +1,33 @@
 package userservice.service;
 
+import userservice.common.*;
+import userservice.domain.*;
+
 @Component
 public class UserServiceImpl implements UserService {
 
+  @Autowired private AuthenticationUtil authUtil;
+  @Autowired private UserRepository userRepository;
+  
   @Override
     public Boolean createUserProfile(UserProfile userProfile) {
-    
+      
+        String salt = authenticationUtil.generateSalt(30);
+        // Generate secure user password 
+        String secureUserPassword = null;
+        try {
+            secureUserPassword = authenticationUtil.generateSecurePassword(userProfile.getUserPassword(), salt);
+        } catch (InvalidKeySpecException ex) {
+            Logger.getLogger(UsersServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        User user = new User();
+        user.setFirstName(userProfile.getFirstName());
+        user.setLastName(userProfile.getLastName());
+        user.setEmail(userProfile.getEmail());
+        user.setPassword(secureUserPassword);
+        userRepository.save(user);
+        
     }
 
 }
