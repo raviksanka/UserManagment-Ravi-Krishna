704d2bddf3554a7cf7316c203f7020df95b4d2bb
Ravi Sanka <33692085+raviksanka@users.noreply.github.com>
2019-11-26 18:54:17 +0530

    Update UserServiceController.java

--- src/main/java/userservice/controller/UserServiceController.java
+++ src/main/java/userservice/controller/UserServiceController.java
@@ -17,5 +17,25 @@ public class UserServiceController {
 	public Boolean createUserProfile(@Valid @RequestBody UserProfile userProfile) {
 		return userService.createUserProfile(userProfile);
 	}
+	
+	@PostMapping("/updateUserProfile")
+	public Boolean updateUserProfile(@Valid @RequestBody UserProfile userProfile) {
+		return userService.updateUserProfile(userProfile);
+	}
+	
+	@GetMapping("/fetchUserByEmail")
+	public UserProfile fetchUserByEmail(String email) {
+		return userService.fetchUserByEmail(email);
+	}
+	
+	@GetMapping("/checkEmailExists")
+	public Boolean checkEmailExists(String email) {
+		return userService.fetchUserByEmail(email);
+	}
+	
+	@PostMapping("/validateLogin")
+	public Boolean validateLogin(@Valid @RequestBody UserProfile userProfile) {
+		return userService.validateLogin(userProfile.getEmail(), userProfile.getPassword());
+	}
   
 }
