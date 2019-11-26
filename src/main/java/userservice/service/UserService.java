77b105bcb2ca22c664f404050c4ec5ddd0d87c1f
Ravi Sanka <33692085+raviksanka@users.noreply.github.com>
2019-11-26 18:45:35 +0530

    Update UserService.java

--- src/main/java/userservice/service/UserService.java
+++ src/main/java/userservice/service/UserService.java
@@ -2,4 +2,8 @@ package userservice.service;
 
 public interface UserService {
     Boolean createUserProfile(UserProfile userProfile);
+    Boolean updateUserProfile(UserProfile userProfile);
+    UserProfile fetchUserByEmail(String email);
+    Boolean checkEmailExists(String email);
+    Boolean validateLogin(String email, String password);
 }
