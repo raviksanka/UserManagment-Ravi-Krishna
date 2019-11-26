32a28ee4909eedd05586ad13f89063e677c0ac4f
Ravi Sanka <33692085+raviksanka@users.noreply.github.com>
2019-11-26 17:13:33 +0530

    Update UserMicroserviceApplication.java

--- src/main/java/userservice/UserMicroserviceApplication.java
+++ src/main/java/userservice/UserMicroserviceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("userservice.properties")
 public class UserMicroserviceApplication {
 
     public static void main(String[] args) {
