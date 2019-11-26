7a707f2252440852b5fa765ebe55c87d2ea7d2d1
Ravi Sanka <33692085+raviksanka@users.noreply.github.com>
2019-11-26 18:20:01 +0530

    Update UserServiceImpl.java

--- src/main/java/userservice/service/UserServiceImpl.java
+++ src/main/java/userservice/service/UserServiceImpl.java
@@ -10,24 +10,67 @@ public class UserServiceImpl implements UserService {
   @Autowired private UserRepository userRepository;
   
   @Override
-    public Boolean createUserProfile(UserProfile userProfile) {
-      
-        String salt = authenticationUtil.generateSalt(30);
-        // Generate secure user password 
-        String secureUserPassword = null;
-        try {
-            secureUserPassword = authenticationUtil.generateSecurePassword(userProfile.getUserPassword(), salt);
-        } catch (InvalidKeySpecException ex) {
-            Logger.getLogger(UsersServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
-        }
-        User user = new User();
+  public Boolean createUserProfile(UserProfile userProfile) {
+
+      String salt = authenticationUtil.generateSalt(30);
+      // Generate secure user password 
+      String secureUserPassword = null;
+      try {
+          secureUserPassword = authenticationUtil.generateSecurePassword(userProfile.getUserPassword(), salt);
+      } catch (InvalidKeySpecException ex) {
+          Logger.getLogger(UsersServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+          return false;
+      }
+      User user = new User();
+      user.setFirstName(userProfile.getFirstName());
+      user.setLastName(userProfile.getLastName());
+      user.setEmail(userProfile.getEmail());
+      user.setPassword(secureUserPassword);
+      userRepository.save(user);
+      return true;
+  }
+  
+  @Override
+  public Boolean updateUserProfile(UserProfile userProfile) {
+
+      User user = userRepository.findByEmail(userProfile.getEmail());
+      if(null != user) {
         user.setFirstName(userProfile.getFirstName());
         user.setLastName(userProfile.getLastName());
-        user.setEmail(userProfile.getEmail());
-        user.setPassword(secureUserPassword);
         userRepository.save(user);
         return true;
-    }
-
+      } else {
+        //throw Exception...
+        //log error...
+        return false;
+      }
+  }
+  
+  @Override
+  public UserProfile fetchUserByEmail(String email) {
+    
+      User user = userRepository.findByEmail(userProfile.getEmail());
+      if(null != user) {
+        UserProfile userProfile = new UserProfile();
+        userProfile.setFirstName(user.getFirstName());
+        userProfile.setLastName(user.getLastName());
+        userProfile.setEmail(user.getEmail());
+        return userProfile;
+      } else {
+        // User with email not found
+        return null;
+      }
+  }
+  
+  @Override
+  public Boolean checkEmailExists(String email) {
+    User user = userRepository.findByEmail(userProfile.getEmail());
+    if(null != user) {
+       return true;
+      } else {
+        // No User yet present with the email
+        return false;
+      }
+  }
+  
 }
